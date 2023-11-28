163ce3f49af87fb6eed79c8870ffc7bf95da7f11
Khachatur <58373478+KhachaturB@users.noreply.github.com>
2023-11-29 01:07:02 +0300

    hw04 - Collections

---
+++ hw04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -0,0 +1,16 @@
+package homework;
+
+import java.util.LinkedList;
+
+public class CustomerReverseOrder {
+
+    private final LinkedList<Customer> list = new LinkedList<>();
+
+    public void add(Customer customer) {
+        list.add(customer);
+    }
+
+    public Customer take() {
+        return list.pollLast();
+    }
+}
